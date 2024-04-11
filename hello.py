f6c7c58312e1a8269b269bd5fa9a20d74de75d3a
JoseA1390 <109625831+JoseA1390@users.noreply.github.com>
2024-04-11 02:02:27 -0500

    Ejemplo 3

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app)
 
 
 @app.route('/')
@@ -10,5 +14,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
+
+
 if __name__ == '__main__':
- app.run(debug=True)
+    manager.run()
